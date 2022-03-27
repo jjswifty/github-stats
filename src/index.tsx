6418653a2f3d035862337e103d58f8945df6d8bd
jjswift <supercheater2018@yandex.ru>
2022-03-27 21:29:44 +0300

    Add apollo client logic: connected graphql into apollo

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ApolloClient, ApolloProvider, createHttpLink, InMemoryCache} from "@apollo/client";
+import {setContext} from "@apollo/client/link/context";
+
+const link = createHttpLink({
+    uri: 'https://api.github.com/graphql',
+    credentials: 'include'
+});
+
+const authLink = setContext((_, {headers}) => ({
+    headers: {
+        ...headers,
+        authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`
+    }
+}));
+
+const client = new ApolloClient({
+    link: authLink.concat(link),
+    cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function

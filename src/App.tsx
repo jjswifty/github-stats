3aeb5f2f229e098e7e68adb710a3d126279bce3c
jjswift <supercheater2018@yandex.ru>
2022-04-04 21:32:56 +0300

    Add simple logic with local query

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import {useMeQuery} from "./generated/graphql";
+import {AuthPage} from "@/ui/pages/auth";
+import {useQuery} from "@apollo/client";
+import {GET_LOGIN} from "@/apollo/queries/local/login";
+import {GetLogin} from "@/apollo/queries/local/__generated__/GetLogin";
 
 
 const App = () => {
-
-    const { data } = useMeQuery({
-        variables: {
-            login: "jjswifty"
-        }
-    })
-
-    console.log(data)
+    const { data } = useQuery<GetLogin>(GET_LOGIN)
+    const login = data?.login.name
 
     return <div className="App">
-
+        {login ? <div>Ваш логин: {login}</div> : <AuthPage/>}
     </div>
 };
 

f6f2d15e6196f53f3b019b0ad7d86c95888ed24c
jjswift <supercheater2018@yandex.ru>
2022-04-08 00:55:03 +0300

    Add browser router, webVitals with console.log and space in import object braces

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {ApolloClient, ApolloProvider, createHttpLink, gql} from "@apollo/client";
-import {setContext} from "@apollo/client/link/context";
-import {cache} from "@/apollo/cache";
+import { ApolloClient, ApolloProvider, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { cache } from "@/apollo/cache";
+import { BrowserRouter } from "react-router-dom";
 
 const link = createHttpLink({
     uri: 'https://api.github.com/graphql',
 });
 
-const authLink = setContext((_, {headers}) => ({
+const authLink = setContext((_, { headers }) => ({
     headers: {
         ...headers,
         authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`
@@ -27,7 +28,9 @@ const client = new ApolloClient({
 ReactDOM.render(
     <React.StrictMode>
         <ApolloProvider client={client}>
-            <App />
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
         </ApolloProvider>
     </React.StrictMode>,
     document.getElementById('root')
@@ -36,4 +39,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log)

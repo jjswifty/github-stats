ca2cb80a89fbd70f24ba32bc4f18d49a6f6b4f1f
jjswift <supercheater2018@yandex.ru>
2022-04-08 02:28:13 +0300

    Add more info to fetch in me query

--- src/apollo/queries/me.ts
+++ src/apollo/queries/me.ts
@@ -4,6 +4,14 @@ export const GET_ME = gql`
     query Me ($login: String!) {
         user (login: $login) {
             bio
+            avatarUrl
+            createdAt
+            starredRepositories {
+                totalCount
+            }
+            repositories {
+                totalCount
+            }
         }
     }
 `

aff1b2496f44d9c78f891a15d26913b04180392c
jjswift <supercheater2018@yandex.ru>
2022-04-08 00:57:39 +0300

    Replace login reactive var with boolean cache field

--- src/apollo/cache.ts
+++ src/apollo/cache.ts
@@ -1,16 +1,30 @@
 import {InMemoryCache} from "@apollo/client";
-import {loginVar} from "@/apollo/queries/variables";
+//import {loginVar} from "@/apollo/queries/variables";
+import {IS_LOGGED_IN} from "@/apollo/queries/local/login";
+import {storage} from "@/lib/storage";
+
 
 export const cache = new InMemoryCache({
-    typePolicies: {
-        Query: {
-            fields: {
-                login: {
-                    read () {
-                        return loginVar()
-                    }
-                },
-            }
-        }
-    }
+//    typePolicies: {
+//        Query: {
+//            fields: {
+//                login: {
+//                    read () {
+//                        return loginVar()
+//                    }
+//                },
+//            }
+//        }
+//    }
 })
+cache.writeQuery({
+    query: IS_LOGGED_IN,
+    data: {
+        isLoggedIn: !!storage.getItem("login"),
+    },
+});
+
+
+
+
+

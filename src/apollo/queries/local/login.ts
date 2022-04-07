5fdfcbf0f320bf6f1718cb693b1fd1bd179405d0
jjswift <supercheater2018@yandex.ru>
2022-04-08 00:58:08 +0300

    Local query to boolean status of login

--- src/apollo/queries/local/login.ts
+++ src/apollo/queries/local/login.ts
@@ -1,10 +1,16 @@
 import {gql} from "@apollo/client";
 
-export const GET_LOGIN = gql`
-    query GetLogin { 
-        login @client {
-            name
-            date
-        }
-    }
+//export const GET_LOGIN = gql`
+//    query GetLogin {
+//        login @client {
+//            name
+//            date
+//        }
+//    }
+//`
+
+export const IS_LOGGED_IN = gql`
+    query isUserLoggedIn {
+        isLoggedIn @client
+    }   
 `

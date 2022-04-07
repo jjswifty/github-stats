84db75c909dd3887558f482a421f97b15b4c1dab
jjswift <supercheater2018@yandex.ru>
2022-04-08 00:56:43 +0300

    Add route system

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,67 @@
-import React from 'react';
-import {AuthPage} from "@/ui/pages/auth";
-import {useQuery} from "@apollo/client";
-import {GET_LOGIN} from "@/apollo/queries/local/login";
-import {GetLogin} from "@/apollo/queries/local/__generated__/GetLogin";
+import React, { ElementType } from 'react';
+import { AuthPage } from "@/ui/pages/auth";
+import { useQuery } from "@apollo/client";
+import { IS_LOGGED_IN } from "@/apollo/queries/local/login";
+import { isUserLoggedIn } from "@/apollo/queries/local/__generated__/isUserLoggedIn";
+import { Route, Routes } from "react-router-dom";
+import { Profile } from "@/ui/pages/profile";
+import { ProtectedRoute } from "@/ui/components/common/ProtectedRoute";
 
+interface IRoute {
+    path: string
+    isAuthRequired: boolean
+    component: ElementType
+    id: number
+}
+
+const ROUTES: Array<IRoute> = [
+    {
+        path: '/auth',
+        isAuthRequired: false,
+        component: AuthPage,
+        id: 0,
+    },
+    {
+        path: '/profile',
+        isAuthRequired: true,
+        component: Profile,
+        id: 1
+    },
+
+]
 
 const App = () => {
-    const { data } = useQuery<GetLogin>(GET_LOGIN)
-    const login = data?.login.name
+
+    const { data } = useQuery<isUserLoggedIn>(IS_LOGGED_IN)
+    const isAuth = data?.isLoggedIn
 
     return <div className="App">
-        {login ? <div>Ваш логин: {login}</div> : <AuthPage/>}
+        <Routes>
+            {
+                isAuth !== undefined && ROUTES.map(e => {
+                    if (e.isAuthRequired) {
+                        return <Route
+                            key={e.id}
+                            path={e.path}
+                            element={
+                                <ProtectedRoute
+                                    isAuthenticated={isAuth}
+                                    authenticationPath="/auth"
+                                    outlet={<e.component />}
+                                />
+                            }
+                        />
+                    }
+                    else {
+                        return <Route
+                            path={e.path}
+                            element={<e.component />}
+                            key={e.id}
+                        />
+                    }
+                })
+            }
+        </Routes>
     </div>
 };
 

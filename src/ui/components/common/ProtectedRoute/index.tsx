bf528fedae09a05d876e2c01c7a83f2a546fcb99
jjswift <supercheater2018@yandex.ru>
2022-04-08 00:53:31 +0300

    Add ProtectedRoute component

---
+++ src/ui/components/common/ProtectedRoute/index.tsx
@@ -0,0 +1,20 @@
+import { Navigate } from "react-router-dom";
+import React from "react";
+
+export type ProtectedRouteProps = {
+    isAuthenticated: boolean;
+    authenticationPath: string;
+    outlet: JSX.Element;
+}
+
+export const ProtectedRoute = ({
+    isAuthenticated,
+    authenticationPath,
+    outlet
+}: ProtectedRouteProps) => {
+    if (isAuthenticated) {
+        return outlet
+    } else {
+        return <Navigate to={{ pathname: authenticationPath }} />
+    }
+}

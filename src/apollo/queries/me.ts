bd36af4cf2190617c74778e2024c01a9f8d7d704
jjswift <supercheater2018@yandex.ru>
2022-04-09 15:16:33 +0300

    Fixed cache merge by requesting id

--- src/apollo/queries/me.ts
+++ src/apollo/queries/me.ts
@@ -3,6 +3,7 @@ import {gql} from "@apollo/client";
 export const GET_ME = gql`
     query Me ($login: String!) {
         user (login: $login) {
+            id
             bio
             avatarUrl
             createdAt
@@ -15,3 +16,6 @@ export const GET_ME = gql`
         }
     }
 `
+
+
+

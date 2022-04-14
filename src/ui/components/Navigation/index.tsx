091eeecabeb215855e5363fef28d9fb32d3b8432
jjswift <supercheater2018@yandex.ru>
2022-04-14 15:06:31 +0300

    Add log out button

--- src/ui/components/Navigation/index.tsx
+++ src/ui/components/Navigation/index.tsx
@@ -1,9 +1,22 @@
-
+import { cache } from "@/apollo/cache";
+import { IS_LOGGED_IN } from "@/apollo/queries/local/login";
+import { isUserLoggedIn } from "@/apollo/queries/local/__generated__/isUserLoggedIn";
+import { storage } from "@/lib/storage";
 
 export const Navigation = () => {
+
+    const logOutHandler = () => {
+        cache.updateQuery<isUserLoggedIn>({
+            query: IS_LOGGED_IN,
+        }, () => ({
+            isLoggedIn: false
+        }))
+        storage.removeItem("login")
+    }
+
     return (
-        <div className="absolute top-15 right-15">
-            sssssss
+        <div className="absolute top-[15px] right-[15px]">
+            <button onClick={logOutHandler}>Log out</button>
         </div>
     )
 }

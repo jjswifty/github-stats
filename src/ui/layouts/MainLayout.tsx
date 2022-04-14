fa3987fe3b57315c3ac9b5daf5c3880a4bdad588
jjswift <supercheater2018@yandex.ru>
2022-04-14 15:06:59 +0300

    default value for background color

--- src/ui/layouts/MainLayout.tsx
+++ src/ui/layouts/MainLayout.tsx
@@ -1,19 +1,27 @@
-import {FC, PropsWithChildren} from "react";
+import React, { FC, PropsWithChildren } from "react";
+import { Navigation } from "@/ui/components/Navigation";
 
 interface MainLayoutProps {
     backgroundColor?: `#${string}` | `bg-${string}`
+    isAuth?: boolean
 }
+
 export const MainLayout: FC<PropsWithChildren<MainLayoutProps>> = ({
     children,
-    backgroundColor
+    backgroundColor= 'bg-main',
+    isAuth
 }) => {
-    console.log(backgroundColor)
     const color = `${backgroundColor?.startsWith('bg-') ? backgroundColor : `bg-[${backgroundColor}]`}`
 
     return (
         // Allows only "bg-somecolor-somestroke" or "#somecolor"
         <div className={`flex items-center justify-center min-h-screen min-w-full ${color}`}>
+            {isAuth && <>
+                <Navigation/>
+
+            </>}
             {children}
         </div>
     )
 }
+
